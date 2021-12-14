d72f35a1a520ce887c7a47c3dce1e3045f21347a
Julio Navarro <julionavarro-09@hotmail.com>
2021-12-14 08:21:56 -0300

    v2

--- src/main/java/com/example/heroes/repository/HeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.heroes.repository;
-
-import com.example.heroes.entity.Hero;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeroRepository extends JpaRepository<Hero,Integer> {
-    
-}
